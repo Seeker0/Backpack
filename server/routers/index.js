3d971568cf2921f78dec72b6d73cd5d448e76909
Jeff Dederick <Xpressivmotion@gmail.com>
2018-01-30 17:35:50 -0500

    passport login to dev

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,9 +1,12 @@
-"use strict";
+'use strict';
 
-const users = require("./users");
-const pouches = require("./pouches");
-const items = require("./items");
+const users = require('./users');
+const pouches = require('./pouches');
+const items = require('./items');
+const login = require('./login');
+const logout = require('./logout');
+const register = require('./register');
 
-const Routes = { users, pouches, items };
+const Routes = { users, pouches, items, login, logout, register };
 
 module.exports = Routes;
---
+++ server/routers/logout.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let router = express.Router();
+
+// ----------------------------------------
+// Route for /logout
+// ----------------------------------------
+
+const onLogout = async (req, res) => {
+  req.logout();
+  req.session.passport.user = null;
+  res.status(200).send();
+};
+
+router.get('/', onLogout);
+
+module.exports = router;
