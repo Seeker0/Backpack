8c62efd2047ee9563071d5e8eec4c3beae3e9349
SamuelLangenfeld <samlangenfeld@gmail.com>
2018-01-31 18:32:00 -0500

    login/logout/items/pouches most routes done

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -4,6 +4,7 @@ import Login from "./Login";
 import SignUp from "./SignUp";
 import Learn from "./Learn";
 import Welcome from "./Welcome";
+import Logout from "./Logout";
 //import Dashboard from "./Dashboard";
 
-export { ErrorMessage, SuccessMessage, Login, SignUp, Learn, Welcome };
+export { ErrorMessage, SuccessMessage, Login, SignUp, Learn, Welcome, Logout };
