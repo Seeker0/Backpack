07449c8919775c6fda3aedcdc6922c60acffca07
Lakshmi <lakshmi.nishthala@gmail.com>
2018-02-01 15:32:36 -0800

    Trying to merge

--- ChromeExtension/content.js
+++
@@ -1,8 +0,0 @@
-//We will change this to work on right-click
-
-
-chrome.runtime.sendMessage({
-    'title': document.title,
-    'url': window.location.href,
-    'summary': window.getSelection().toString()
-});
--- ChromeExtension/storelinks.js
+++ ChromeExtension/storelinks.js
@@ -1,7 +1,8 @@
-function onPageDetailsReceived(pageDetails)  { 
-    document.getElementById('title').value = pageDetails.title; 
-    document.getElementById('url').value = pageDetails.url; 
-    document.getElementById('summary').innerText = pageDetails.summary; 
+function onPageDetailsReceived(pageDetails) {
+  //chrome.runtime.getBackgroundPage().console.log(pageDetails);
+  document.getElementById("title").value = pageDetails.title;
+  document.getElementById("url").value = pageDetails.url;
+  document.getElementById("summary").innerText = pageDetails.summary;
 }
 
 // Global reference to the status display SPAN
@@ -9,65 +10,73 @@ var statusDisplay = null;
 
 // POST the data to the server using XMLHttpRequest
 function addBookmark() {
-    // Cancel the form submit
-    event.preventDefault();
+  // Cancel the form submit
+  event.preventDefault();
 
-    // The URL to POST our data to
-    var postUrl = 'http://localhost:3001/items';
+  // The URL to POST our data to
+  var postUrl = "http://localhost:3001/items";
 
-    // Set up an asynchronous AJAX POST request
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', postUrl, true);
+  // Set up an asynchronous AJAX POST request
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", postUrl, true);
 
-    // Prepare the data to be POSTed by URLEncoding each field's contents
-    var title = encodeURIComponent(document.getElementById('title').value);
-    var url = encodeURIComponent(document.getElementById('url').value);
-    var summary = encodeURIComponent(document.getElementById('summary').value);
-    var tags = encodeURIComponent(document.getElementById('tags').value);
+  // Prepare the data to be POSTed by URLEncoding each field's contents
+  var title = encodeURIComponent(document.getElementById("title").value);
+  var url = encodeURIComponent(document.getElementById("url").value);
+  var summary = encodeURIComponent(document.getElementById("summary").value);
+  var tags = encodeURIComponent(document.getElementById("tags").value);
 
-    var params = 'title=' + title + 
-                 '&url=' + url + 
-                 '&summary=' + summary +
-                 '&tags=' + tags;
+  var params =
+    "title=" + title + "&url=" + url + "&summary=" + summary + "&tags=" + tags;
 
-    // Replace any instances of the URLEncoded space char with +
-    params = params.replace(/%20/g, '+');
+  // Replace any instances of the URLEncoded space char with +
+  params = params.replace(/%20/g, "+");
 
-    // Set correct header for form data 
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  // Set correct header for form data
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-    // Handle request state change events
-    xhr.onreadystatechange = function() { 
-        // If the request completed
-        if (xhr.readyState == 4) {
-            statusDisplay.innerHTML = '';
-            if (xhr.status == 200) {
-                
-                statusDisplay.innerHTML = 'Saved!';
-                window.setTimeout(window.close, 1000);
-            } else {
-                
-                statusDisplay.innerHTML = 'Error saving: ' + xhr.statusText;
-            }
-        }
-    };
+  // Handle request state change events
+  xhr.onreadystatechange = function() {
+    // If the request completed
+    if (xhr.readyState == 4) {
+      statusDisplay.innerHTML = "";
+      if (xhr.status == 200) {
+        statusDisplay.innerHTML = "Saved!";
+        window.setTimeout(window.close, 1000);
+      } else {
+        statusDisplay.innerHTML = "Error saving: " + xhr.statusText;
+      }
+    }
+  };
 
-    // Send the request and set status
-    xhr.send(params);
-    statusDisplay.innerHTML = 'Saving...';
+  // Send the request and set status
+  xhr.send(params);
+  statusDisplay.innerHTML = "Saving...";
 }
 
 // When the popup HTML has loaded
-window.addEventListener('load', function(evt) {
-    // Cache a reference to the status display SPAN
-    statusDisplay = document.getElementById('status-display');
-    // Handle the bookmark form submit event with our addBookmark function
-    document.getElementById('addbookmark').addEventListener('submit', addBookmark);
-    // Get the event page
-    chrome.runtime.getBackgroundPage(function(eventPage) {
-        // Call the getPageInfo function in the event page, passing in 
-        // our onPageDetailsReceived function as the callback. This injects 
-        // content.js into the current tab's HTML
-        eventPage.getPageDetails(onPageDetailsReceived);
+window.addEventListener("load", function(evt) {
+  // Cache a reference to the status display SPAN
+  statusDisplay = document.getElementById("status-display");
+  // Handle the bookmark form submit event with our addBookmark function
+  document
+    .getElementById("addbookmark")
+    .addEventListener("submit", addBookmark);
+  // Get the event page
+  //chrome.runtime.getBackgroundPage(function(eventPage) {
+  // Call the getPageInfo function in the event page, passing in
+  // our onPageDetailsReceived function as the callback. This injects
+  // content.js into the current tab's HTML
+  //eventPage.getPageDetails(onPageDetailsReceived);
+  chrome.runtime.onMessage.addListener(onPageDetailsReceived);
+  document.oncontextmenu = e => {
+    //url = e.target.href;
+    console.log(e.target.href);
+    //return url;
+    chrome.runtime.sendMessage({
+      title: document.title,
+      summary: window.getSelection().toString(),
+      url: e.target.href
     });
+  };
 });
