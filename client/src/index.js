0be2c257626de959f59297cb2e0c4026af73ce93
SamuelLangenfeld <samlangenfeld@gmail.com>
2018-02-03 11:33:17 -0500

    authorization and redirects work correctly

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import AppContainer from "./Containers/AppContainer";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "./CSS/index.css";
-
+import { BrowserRouter as Router } from "react-router-dom";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 
@@ -16,7 +16,9 @@ let store = createStore(currentUser, applyMiddleware(logger, thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <AppContainer />
+    <Router>
+      <AppContainer />
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
