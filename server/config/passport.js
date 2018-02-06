9967a71e3cf047cc8e1ffc33ce51abb60e46caac
Jeff Dederick <Xpressivmotion@gmail.com>
2018-02-06 08:59:41 -0500

    no merge conflicts. Functionality remains intact

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,17 +1,17 @@
-const passport = require("passport");
-const FacebookStrategy = require("passport-facebook").Strategy;
-const LocalStrategy = require("passport-local").Strategy;
-const User = require("../models/user");
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
 
 passport.use(
   new LocalStrategy((username, password, done) => {
     User.findOne({ username: username }, (err, user) => {
       if (err) return done(err);
       if (!user) {
-        return done(null, false, { message: "Invalid Username!" });
+        return done(null, false, { message: 'Invalid Username!' });
       }
       if (!user.validPassword(password)) {
-        return done(null, false, { message: "Invalid Password!" });
+        return done(null, false, { message: 'Invalid Password!' });
       }
 
       return done(null, user);
@@ -19,28 +19,28 @@ passport.use(
   })
 );
 
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: "http://localhost:3000/login/facebook/callback",
-      profileFields: ["id", "displayName", "email"]
-    },
-    async function(accessToken, refreshToken, profile, cb) {
-      try {
-        let user = await User.findOrCreate({ email: profile.emails[0].value });
-        user.facebookId = profile.id;
-        user.username = profile.displayName;
-        user = await user.save();
-        return cb(null, user);
-      } catch (e) {
-        console.error(e);
-        cb(e);
-      }
-    }
-  )
-);
+// passport.use(
+//   new FacebookStrategy(
+//     {
+//       clientID: process.env.FACEBOOK_APP_ID,
+//       clientSecret: process.env.FACEBOOK_APP_SECRET,
+//       callbackURL: "http://localhost:3000/login/facebook/callback",
+//       profileFields: ["id", "displayName", "email"]
+//     },
+//     async function(accessToken, refreshToken, profile, cb) {
+//       try {
+//         let user = await User.findOrCreate({ email: profile.emails[0].value });
+//         user.facebookId = profile.id;
+//         user.username = profile.displayName;
+//         user = await user.save();
+//         return cb(null, user);
+//       } catch (e) {
+//         console.error(e);
+//         cb(e);
+//       }
+//     }
+//   )
+// );
 
 passport.serializeUser((user, done) => {
   done(null, user._id);
