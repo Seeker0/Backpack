51b16ccd68e4422c6a5b4fbac398a3104948a7ea
SamuelLangenfeld <samlangenfeld@gmail.com>
2018-02-08 10:45:51 -0500

    allow cross-origin

--- server/app.js
+++ server/app.js
@@ -115,6 +115,24 @@ const loggedOutOnly = (req, res, next) => {
 };
 
 // ----------------------------------------
+// Cors Middleware
+// ----------------------------------------
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+// ----------------------------------------
 // Routes
 // ----------------------------------------
 
