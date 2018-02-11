3c12b7eeb1ab7a8b6d277a6ab7258510336acfc3
SamuelLangenfeld <samlangenfeld@gmail.com>
2018-02-11 07:24:29 -0500

    user heroku app ids

--- client/src/Components/FacebookLoginButton.js
+++ client/src/Components/FacebookLoginButton.js
@@ -4,7 +4,7 @@ import FacebookLogin from "react-facebook-login";
 const FacebookLoginButton = props => {
   return (
     <FacebookLogin
-      appId="1931646120486917"
+      appId="2054896458089762"
       autoLoad={false}
       fields="name,email,picture"
       callback={props.facebookLogin}
@@ -14,29 +14,3 @@ const FacebookLoginButton = props => {
 };
 
 export default FacebookLoginButton;
-
-/*
-const FacebookLoginButton = props => {
-  return (
-    <button className="facebook-button" onClick={props.facebookLogin}>
-      LOGIN WITH FACEBOOK
-    </button>
-  );
-};
-*/
-
-/*
-
-<FacebookLogin
-      socialId="1931646120486917"
-      language="en_US"
-      scope="public_profile,email,name"
-      responseHandler={props.facebookLogin}
-      xfbml={true}
-      fields="id,email"
-      version="v2.5"
-      className="facebook-login"
-      buttonText="Login With Facebook"
-    />
-
-    */
--- client/src/Components/GoogleLoginButton.js
+++ client/src/Components/GoogleLoginButton.js
@@ -7,7 +7,7 @@ const GoogleLoginButton = props => {
   return (
     <SocialButton
       provider="google"
-      appId="1059973407731-77vgqju0ric1itqtuaol5mv65vvk3uet.apps.googleusercontent.com"
+      appId="17204089583-su0tdc6ppndkf8eacncljcveuv6o1olo.apps.googleusercontent.com"
       onLoginSuccess={props.googleLogin}
       onLoginFailure={props.googleLogin}
       className="google-button"
